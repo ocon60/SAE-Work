f5cab66689ad52183066aab18043e5740b556fa3
Christian <chrisocon60@gmail.com>
2014-01-23 01:17:11 -0500

    pushing rosterview

---
+++ rosterview/roster.js
@@ -0,0 +1,28 @@
+/*----------------SWITCH CLASSES--------------*/
+$(document).ready(function(){
+  $(".button").click(function(){
+    $("#currRos").fadeOut("slow",function() {
+    	$("currRos").fadeIn("fast")
+    });
+  });
+});
+
+/*---------------PULL INFO---------------------*/
+var imgURL = "img/";
+var bioURL = "bios/";
+
+var dest = ".carousel-inner";
+
+var fileextension = ".png";
+
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: imgURL,
+    success: function (data) {
+        //List all png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            $("body").append($("<img src=" + Dir + filename + "></img>"));
+        });
+    }
+});
