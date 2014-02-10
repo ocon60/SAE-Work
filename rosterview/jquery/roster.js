49e57db0ff914392f80fe840c4818d32bd15e03d
Christian <chrisocon60@gmail.com>
2014-02-10 18:23:54 -0500

    Added cool hover-over effects

--- rosterview/jquery/roster.js
+++ rosterview/jquery/roster.js
@@ -1,28 +1,59 @@
-/*----------------SWITCH CLASSES--------------*/
-$(document).ready(function(){
-  $(".button").click(function(){
-    $("#currRos").fadeOut("slow",function() {
-    	$("currRos").fadeIn("fast")
+$(document).ready(function() {
+
+//-----------Darken DIV on mouseenter, lighten on mouseout--------//
+//#roster-frame
+$("#roster-frame").on("mouseenter",function() {
+    $(this).fadeTo("fast", 0.9, function() {
+        console.log("roster-frame darkened to 1.0");
     });
-  });
+});
+$("#roster-frame").on("mouseout",function() {
+    $(this).fadeTo("fast", 0.5, function() {
+        console.log("roster-frame lightened back to 0.5");
+    });
+});
+//#infopane-container
+$("#infopane-container").on("mouseenter",function() {
+    $("#bio-container").fadeTo("fast",0.9,function() {
+        console.log("bio-container darkened to 1.0")
+    });
+    $("#pic-container").fadeTo("fast", 0.9, function() {
+        console.log("pic-container darkened to 1.0");
+    });
+    $("#links-container").fadeTo("fast", 0.9, function() {
+        console.log("links-container darkened to 1.0");
+    });
+});
+$("#infopane-container").on("mouseout",function() {
+    $("#bio-container").fadeTo("fast",0.5,function() {
+        console.log("bio-container lightened back to 0.5");
+    });
+    $("#pic-container").fadeTo("fast", 0.5, function() {
+        console.log("pic-container lightened back to 0.5");
+    });
+    $("#links-container").fadeTo("fast", 0.5, function() {
+        console.log("links-container lightened back to 0.5");
+    });
+});
+//------Shadow on mouseenter, disappear on mouseout----//
+//#roster-frame
+$("#roster-frame").on("mouseenter", function() {
+    $(this).css("box-shadow", "15px 15px 10px #0c0c0c");
+});
+$("#roster-frame").on("mouseout", function() {
+    $(this).css("box-shadow", "none");
+});
+//#infopane-container
+$("#infopane-container").on("mouseenter",function() {
+    $("#bio-container").css("box-shadow", "15px 15px 10px #060606");
+    $("#pic-container").css("box-shadow", "15px 15px 10px #060606");
+    $("#links-container").css("box-shadow", "15px 15px 10px #060606");
+});
+$("#infopane-container").on("mouseout",function() {
+    $("#bio-container").css("box-shadow", "none");
+    $("#pic-container").css("box-shadow", "none");
+    $("#links-container").css("box-shadow", "none");
 });
 
-/*---------------PULL INFO---------------------*/
-var imgURL = "img/";
-var bioURL = "bios/";
 
-var dest = ".carousel-inner";
-
-var fileextension = ".png";
-
-$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: imgURL,
-    success: function (data) {
-        //List all png file names in the page
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-            $("body").append($("<img src=" + Dir + filename + "></img>"));
-        });
-    }
 });
