38a7889ff8ba4063ae53a5ec9823ef7321470386
Christian <chrisocon60@gmail.com>
2014-02-18 18:21:36 -0500

    working on server.js for pg connection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//---------------STATIC HTML SERVER-----------------------------------------------------//
+
 var express = require('express'),
   app = express();
 
@@ -10,13 +12,25 @@ console.log("App listening on port 5000");
 var d = new Date();
 console.log("Date: "+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDay()+" "+d.get);
 
- var pg = require('pg');
+//----------------TEST CONNECT TO DATABASE----------------------------------------------------//
 
-pg.connect(process.env.DATABASE_URL, function(err ,client) {
-	var query = client.query('SELECT * FROM d454hdqpfimhl9.rostertable');
-	
-	query.on('row', function(row) {
-		console.log(JSON.stringify(row));
-		
-	});
-}); 
+var pg = require('pg'); 
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = "postgres://eefyabuavpdzzd:hTM0hvHvKQvik9NxA9jY0iXjwu@ec2-174-129-218-200.compute-1.amazonaws.com:5432/d454hdqpfimhl9";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
