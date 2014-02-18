de7a39c93997a585ddd543162dd4bec8e08d5e70
ocon60 <chrisocon60@gmail.com>
2014-02-17 22:27:31 -0500

    Adding postgresql to server.js

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ app.listen(process.env.PORT || 5000);
 console.log("App listening on port 5000");
 var d = new Date();
 console.log("Date: "+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDay()+" "+d.get);
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err ,client) {
+	var query = client.query('SELECT * FROM rostertable');
+	
+	query.on('row', function(row) {
+		console.log(JSON.stringify(row));
+		
+	});
+});
