42e7b6fd7dcf33a4f9af2bd3e131a3ee820ad8ce
Christian <chrisocon60@gmail.com>
2014-02-20 13:08:48 -0500

    added db test connect to routing file

--- handlers.js
+++ handlers.js
@@ -1,17 +1,15 @@
-fs = require('fs');
-fs.readFile('');
+//-------------------------TEST CONNECT TO DATABASE----------------------------------------------//
+var pg = require('pg'); 
+
+var conString = "postgres://eefyabuavpdzzd:hTM0hvHvKQvik9NxA9jY0iXjwu@ec2-174-129-218-200.compute-1.amazonaws.com:5432/d454hdqpfimhl9";
+
+var testClient = new Client({user: 'eefyabuavpdzzd', database: 'd454hdqpfimhl9'});
+client.on('drain', client.end.bind(client));
+testClient.connect();
+
+var testQuery = testClient.query("SHOW TABLES", function(err, result) {
+	console.log(result);
+})
 
 
-var rosterfill() {
-	fs.readdir('/bios', function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		console.log(data);
-		exports.rosterFill = function() {
-			return data;
-		}
-	} )
-
-}
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
 	app = express();
 	port = parseInt(process.env.PORT, 10)|| 5000;
 
+app.use(app.router);
 app.get("/", function(req,res) {
 	res.redirect("rosterview/index.html");	
 });
@@ -16,24 +17,7 @@ app.configure(function(){
     dumpExceptions: true, 
     showStack: true
   }));
-  app.use(app.router);
+  
 });
 
 app.listen(port);
-
-
-//-------------------------TEST CONNECT TO DATABASE----------------------------------------------//
-
-var pg = require('pg'); 
-//or native libpq bindings
-//var pg = require('pg').native
-var conString = "postgres://eefyabuavpdzzd:hTM0hvHvKQvik9NxA9jY0iXjwu@ec2-174-129-218-200.compute-1.amazonaws.com:5432/d454hdqpfimhl9";
-
-var testClient = new Client({user: 'eefyabuavpdzzd', database: 'd454hdqpfimhl9'});
-client.on('drain', client.end.bind(client));
-testClient.connect();
-
-var testQuery = testClient.query("SHOW TABLES", function(err, result) {
-	console.log(result);
-})
-
