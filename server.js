f2b5ffb4ebd36fc8d4a881d75802dea5fb409bc6
Christian <chrisocon60@gmail.com>
2014-02-20 12:16:46 -0500

    created test connect in server.js

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-//---------------STATIC HTML SERVER-----------------------------------------------------//
+//---------------------------STATIC HTML SERVER-----------------------------------------------------//
 
 var express = require('express'),
-  app = express();
+	app = express();
+	port = parseInt(process.env.PORT, 10)|| 5000;
+
+app.get("/", function(req,res) {
+	res.redirect("rosterview/index.html");	
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/rosterview'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(port);
 
 
-app.use(express.static(__dirname + '/rosterview'));
-
-app.listen(process.env.PORT || 5000);
-
-console.log("App listening on port 5000");
-var d = new Date();
-console.log("Date: "+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDay()+" "+d.get);
-
-//----------------TEST CONNECT TO DATABASE----------------------------------------------------//
+//-------------------------TEST CONNECT TO DATABASE----------------------------------------------//
 
 var pg = require('pg'); 
 //or native libpq bindings
 //var pg = require('pg').native
-
 var conString = "postgres://eefyabuavpdzzd:hTM0hvHvKQvik9NxA9jY0iXjwu@ec2-174-129-218-200.compute-1.amazonaws.com:5432/d454hdqpfimhl9";
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
-});
+var testClient = new Client({user: 'eefyabuavpdzzd', database: 'd454hdqpfimhl9'});
+client.on('drain', client.end.bind(client));
+testClient.connect();
+
+var testQuery = testClient.query("SHOW TABLES", function(err, result) {
+	console.log(result);
+})
+
