83623bfc10d747edf69b0d8ba8836e1ceb2f5861
ocon60 <chrisocon60@gmail.com>
2014-03-20 14:45:23 -0400

    testing offline connection on bus

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,34 @@
 // Dynamically includes all routes in a sub directory (routes/index.js)
 // Credit goes to stackoverflow.com/users/1101154/kostia 
 //                stackoverflow.com/users/2351/sam-corder
+//                stackoverflow.com/users/1437845/tandrewnichols
 
+function loadRoutes(folder){
+    if (!folder){
+        folder = __dirname + '/routes/';
+    }
 
-var fs = require('fs');
+    fs.readdir(folder, function(err, files){
+        var l = files.length;
+        for (var i = 0; i < l; i++){
+            var file = files[i];
+            fs.stat(file, function(err, stat){
+                if (stat && stat.isDirectory()){
+                    loadRoutes(folder + '/' + file + '/');
+                } else {
+                    var dot = file.lastIndexOf('.');
+                    if (file.substr(dot + 1) === 'js'){
+                        var name = file.substr(0, dot);
 
-module.exports = function(app) {
-	fs.readdirSync(__dirname).forEach(function(file) {
-		if (file == "index.js") return;
-		var name = file.substr(0, file.indexOf('.'));
-		require('./' + name)(app);
-	});
+                        // I'm also passing argv here (from optimist)
+                        // so that I can easily enable debugging for all
+                        // routes.
+                        require(folder + name)(app, argv);
+                    }
+                }
+            });
+        }
+    });
 }
 
 
